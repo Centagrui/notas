4bfca1d20baa58ef7f75a969a2dc76d7d9677f4f
karlaQuintanaR <s22120215@alumnos.itsur.edu.mx>
2025-10-08 00:09:27 -0600

    Se arreglo problemas para que la applicacion se mostrara en la pantalla en el MainActivity

---
+++ app/src/main/java/com/example/notas/ui/theme/viewmodel/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.notas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.notas.navigation.AppNavigation
+import com.example.notas.ui.theme.NotasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContent lo usamos para que se mostrara ya la pantalla
+        setContent {
+            NotasTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    AppNavigation(navController = navController)
+                }
+            }
+        }
+    }
+}
